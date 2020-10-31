9945b7fc8fe20a1a54ab0c585793ad51f7ae284c
lakshmikorukanti <luckyrao4e8@gmail.com>
2020-10-31 18:25:16 +0530

    Updates

---
+++ precache-manifest.8ec8a1956d70cd17c4298cd0e281a420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0139595045c58cc34aabbf0759512553",
+    "url": "/Expense-manager/index.html"
+  },
+  {
+    "revision": "94bf86ffe894bc95eda6",
+    "url": "/Expense-manager/static/css/main.54074ebb.chunk.css"
+  },
+  {
+    "revision": "7258dff26c5cbe0dcdd2",
+    "url": "/Expense-manager/static/js/2.1bd6368d.chunk.js"
+  },
+  {
+    "revision": "639c28c8ef14bcd37d017fe8f6dc04e2",
+    "url": "/Expense-manager/static/js/2.1bd6368d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bf86ffe894bc95eda6",
+    "url": "/Expense-manager/static/js/main.6a30bbc7.chunk.js"
+  },
+  {
+    "revision": "7fcecded07d97b202312",
+    "url": "/Expense-manager/static/js/runtime-main.4ba657f2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Expense-manager/static/media/logo.ee7cd8ed.svg"
+  }
+]);
